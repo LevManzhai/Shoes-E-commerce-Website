39bd31693b66654cd54e2b0bced2e9b994c16cb8
Lev <levmanzh@gmail.com>
2025-09-17 19:21:08 +0100

    Configure Vite for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  base: '/Shoes-E-commerce-Website/',
+  build: {
+    assetsDir: 'assets',
+    outDir: 'dist'
+  },
   server: {
     port: 3001
   }
