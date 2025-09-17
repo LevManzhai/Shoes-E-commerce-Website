f4bf3d750fe86d966e853e4a84649e6d8c0a15d4
Lev <levmanzh@gmail.com>
2025-09-17 20:31:09 +0100

    Fix all image paths for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -9,8 +9,14 @@ export default defineConfig({
     outDir: 'dist',
     rollupOptions: {
       external: [
-        '/images/testimonials/persone-1.jpg',
-        '/images/testimonials/persone-2.jpg'
+        'images/testimonials/persone-1.jpg',
+        'images/testimonials/persone-2.jpg',
+        'images/shoes/shoes-1.png',
+        'images/shoes/shoes-2.png',
+        'images/shoes/shoes-3.png',
+        'images/shoes/shoes-4.png',
+        'images/shoes/shoes-5.png',
+        'images/shoes/shoes-6.png'
       ]
     }
   },
